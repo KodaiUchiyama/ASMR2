6f2bf832ad2319f38a8e2350545964b61085647e
KodaiUchiyama <xtraxtra.l.one@gmail.com>
2021-02-10 01:53:13 +0900

    既存のデータセットログtxtを整理，backupフォルダに入れる

--- data/AV_log/gentxtnew.py
+++ data/AV_log/gentxtnew.py
@@ -1,3 +1,16 @@
+with open('../audio/AV_model_database/single_TF.txt', 'r') as t:
+    lines = t.readlines()
+    for line in lines:
+        info = line.strip().split('.')[0] # 0-0
+        #num1 = info[0].strip().split('-')[1]
+        #num2 = info[0].strip().split('-')[2]
+
+        #newline = line.strip() + ' ' + num1 + '_face_emb.npy' + ' ' + num2 + '_face_emb.npy\n'
+        newline = line.strip() + ' ' + info + '_face_emb.npy\n' # 0-0.npy 0-0_face_emb.npy
+        with open('AVdataset_train.txt', 'a') as f:
+            f.write(newline)
+
+''' 訓練データと検証データをlogデータからランダムに分ける場合
 with open('../audio/AV_model_database/dataset_train.txt', 'r') as t:
     lines = t.readlines()
     for line in lines:
@@ -21,3 +34,4 @@ with open('../audio/AV_model_database/dataset_val.txt', 'r') as t:
         newline = line.strip() + ' ' + info + '_face_emb.npy\n' # 0-0.npy 0-0_face_emb.npy
         with open('AVdataset_val.txt', 'a') as f:
             f.write(newline)
+'''
--- data/AV_log/gentxtnew_test.py
+++ data/AV_log/gentxtnew_test.py
@@ -1,12 +1,24 @@
-with open('../audio/AV_model_database_test/single_TF.txt', 'r') as t:
+'''
+with open('../audio/AV_model_database_expanded_test/single_TF.txt', 'r') as t:
     lines = t.readlines()
     for line in lines:
         info = line.strip().split('.')[0] # 0-0
-        #num1 = info[0].strip().split('-')[1]
-        #num2 = info[0].strip().split('-')[2]
 
-        #newline = line.strip() + ' ' + num1 + '_face_emb.npy' + ' ' + num2 + '_face_emb.npy\n'
         newline = line.strip() + ' ' + info + '_face_emb.npy\n' # 0-0.npy 0-0_face_emb.npy
-        with open('AVdataset_test.txt', 'a') as f:
+        with open('AVdataset_expanded_test.txt', 'a') as f:
             f.write(newline)
+'''
+with open('../audio/AV_model_database_expanded_test/single_TF.txt', 'r') as t:
+    lines = t.readlines()
+    for line in lines:
+        info = line.strip().split('.')[0] # 0-0
+        # 交差検証を行うためtestデータの名称が，trainデータと重複するしているのを修正する
+        # video index 0->9 を 35->44に変更
+        num1Str = info.split('-')[0]
+        num2Str = info.split('-')[1]
+        #string をint変換して足す
+        num1Str = str(int(num1Str) + 35)
 
+        newline = num1Str + '-' + num2Str + '.npy' + ' ' + num1Str + '-' + num2Str + '_face_emb.npy\n' # 35-0.npy 35-0_face_emb.npy
+        with open('AVdataset_expanded_test.txt', 'a') as f:
+            f.write(newline)
